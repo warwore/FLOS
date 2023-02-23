17e9e36809dc1412008d19592ac753498ab68e7c
Warwore Omot <omotwarwore@gmail.com>
2023-02-22 23:35:26 -0500

    movement

--- FLOS.py
+++ FLOS.py
@@ -1,118 +1,148 @@
+# Import necessary modules
 import pygame
 from sys import exit
-import numpy as np
-pygame.init() 
 
-#Game Window
+# Initialize pygame
+pygame.init()
+
+# Define constants
 WIDTH = 800
 HEIGHT = 400
+FPS = 60
+FLEET_RECT = pygame.Rect(15, 15, 120, 100)
+DELIVERY_RECT = pygame.Rect(30, 150, 120, 225)
+FACTORY_MAP_RECT = pygame.Rect(150, 15, 630, 360)
+AGV_START_POS = (150, 300)
+AGV_SPEED = 0.2
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
-pygame.display.set_caption('AGV Simulator') 
+# OBSTACLES = [pygame.Rect(200, 100, 50, 50), pygame.Rect(400, 200, 100, 75)]
+# PICKUP_POINTS = [pygame.Rect(50, 50, 20, 20), pygame.Rect(500, 150, 30, 30)]
+# DROP_OFF_POINTS = [pygame.Rect(700, 50, 20, 20), pygame.Rect(250, 300, 30, 30)]
+
+# Create game window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('AGV Simulator')
+
 clock = pygame.time.Clock() #for setting fps
-font = pygame.font.Font('graphics/Arial.ttf', 15) 
 
+# Load fonts
+font = pygame.font.Font('graphics/Arial.ttf', 15)
 
-#Creating background
-fleet_rect = pygame.Rect(15, 15, 120, 100)  #left,top,width,height
-delivery_rect = pygame.Rect(30, 150, 120, 225)
-factory_map_rect = pygame.Rect(150, 15, 630, 360)
+# Load images
+agv_surf = pygame.image.load('graphics/agv.png').convert_alpha()
+agv_surf = pygame.transform.scale(agv_surf, (20, 20))
 
-#Creating text
-text_fleet_surf = font.render('Fleet', False, 'Black')
-text_fleet_rect = text_fleet_surf.get_rect(topleft = (55,0))
-
-text_delivery_surf = font.render('Delivery', False, 'Black')
-text_delivery_rect = text_delivery_surf.get_rect(topleft = (45,135))
-
-text_factory_map_surf = font.render('Factory Map', False, 'Black')
-text_factory_map_rect = text_factory_map_surf.get_rect(topleft = (400,0))
-
-
-#Creating AGV
-agv_start_pos = (150, 300)
-agv_surf = pygame.image.load('graphics/agv.png').convert_alpha() 
-agv_surf = pygame.transform.scale(agv_surf, (20,20)) #making agv smaller
-agv_rect = agv_surf.get_rect(topleft = agv_start_pos) #converting to rect
-
-#Draw the backround stuff
+# Define functions
 def draw_bg():
+    """Draws the background of the game window."""
     screen.fill('White')
-    pygame.draw.rect(screen, 'Black', fleet_rect, 2)
-    pygame.draw.rect(screen, 'Black', delivery_rect, 2)
-    pygame.draw.rect(screen, 'Black', factory_map_rect, 2)
+    pygame.draw.rect(screen, 'Black', FLEET_RECT, 2)
+    pygame.draw.rect(screen, 'Black', DELIVERY_RECT, 2)
+    pygame.draw.rect(screen, 'Black', FACTORY_MAP_RECT, 2)
 
-#Draw the text
+    # Draw obstacles
+    # for obstacle in OBSTACLES:
+    #     pygame.draw.rect(screen, 'Red', obstacle)
+
+    # # Draw pickup points
+    # for pickup_point in PICKUP_POINTS:
+    #     pygame.draw.rect(screen, 'Green', pickup_point)
+
+    # # Draw drop off points
+    # for drop_off_point in DROP_OFF_POINTS:
+    #     pygame.draw.rect(screen, 'Blue', drop_off_point)
+
+
 def draw_text():
-    #placing text on window
-    screen.blit(text_factory_map_surf,text_factory_map_rect)
-    screen.blit(text_fleet_surf,text_fleet_rect)
+    """Draws the text on the game window."""
+    text_fleet_surf = font.render('Fleet', False, 'Black')
+    text_fleet_rect = text_fleet_surf.get_rect(topleft=(55, 0))
+    screen.blit(text_fleet_surf, text_fleet_rect)
+
+    text_delivery_surf = font.render('Delivery', False, 'Black')
+    text_delivery_rect = text_delivery_surf.get_rect(topleft=(45, 135))
     screen.blit(text_delivery_surf, text_delivery_rect)
 
-#Draw the agv
+    text_factory_map_surf = font.render('Factory Map', False, 'Black')
+    text_factory_map_rect = text_factory_map_surf.get_rect(topleft=(400, 0))
+    screen.blit(text_factory_map_surf, text_factory_map_rect)
+
 def draw_agv():
-    #placing agv on window
+    """Draws the AGV on the game window."""
     screen.blit(agv_surf, agv_rect)
 
-#Makes a new list with corrected coordinates
-corrected = []    
 def autocorrect(lines):
+    """Autocorrects the lines to make them straight."""
+    corrected = []
     if done_line:
         corrected.append(lines[0])
         for i in range(0, len(lines), 2):
-            if (abs(lines[i][0] - lines[i+1][0]) > abs(lines[i][1] - lines[i+1][1])):
-                corrected.append((lines[i+1][0], lines[i][1]))
-                corrected.append((lines[i+1][0], lines[i][1]))
+            if abs(lines[i][0] - lines[i + 1][0]) > abs(lines[i][1] - lines[i + 1][1]):
+                corrected.append((lines[i + 1][0], lines[i][1]))
+                corrected.append((lines[i + 1][0], lines[i][1]))
             else:
-                corrected.append((lines[i][0], lines[i+1][1]))
-                corrected.append((lines[i][0], lines[i+1][1]))
+                corrected.append((lines[i][0], lines[i + 1][1]))
+                corrected.append((lines[i][0], lines[i + 1][1]))
         del corrected[len(corrected) - 1]
+    return corrected
 
-
-
-lines = []
-start = agv_start_pos
-done_line = False;
-#Draws the lines from a list
-def draw_lines(x):    
+def draw_lines(x):
+    """Draws the lines on the game window."""
     if done_line:
-        for i in range(0, len(x), 2): #change to while?
-            pygame.draw.line(screen,'Gold', x[i], x[i+1]) #Need a start coordinate and end coordinate to draw a line     
-    
+        for i in range(0, len(x), 2):
+            pygame.draw.line(screen, 'Gold', x[i], x[i + 1])
+
+
+# Initialize variables
+lines = []
+start = AGV_START_POS
+done_line = False
+current_path_index = 0
+agv_rect = agv_surf.get_rect(topleft=AGV_START_POS)
+moving = False
+
 while True:
     for event in pygame.event.get():
-        
-        if event.type == pygame.QUIT: #Checking if the window is trying to be closed
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
-        
-        # press a to save a coordinate
-        
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                end = pygame.mouse.get_pos() #Get the coordinates of cursor location
+                end = pygame.mouse.get_pos()
                 lines.append(start)
                 lines.append(end)
                 start = end
                 print('Line saved')
-                print('Current Coordiantes saved', lines)
-                
-        #Start drawing the lines on the screen after pressing 'd' key    
-        # press d to start drawing the lines on the screen
-        if event.type == pygame.KEYDOWN:
+                print('Current coordinates saved:', lines)
+
             if event.key == pygame.K_d:
-                print('Now Drawing ROUTE')
+                print('Now drawing route')
                 done_line = True
-    
-    
-                                         
+
+            if event.key == pygame.K_r:
+                current_path_index = 0
+                agv_rect.topleft = agv_start_pos
+                moving = False
+
     draw_bg()
     draw_text()
-    autocorrect(lines) #make straiiiight lines
-    draw_lines(corrected)
+
+    if done_line:
+        corrected = autocorrect(lines)
+        if current_path_index < len(corrected) - 1:
+            current_path_index += AGV_SPEED
+            moving = True
+        else:
+            done_line = False
+            current_path_index = 0
+            moving = False
+
+    if moving:
+        agv_rect.topleft = corrected[int(current_path_index)]
+        draw_lines(corrected)
+
     draw_agv()
 
-    
     pygame.display.update()
-    clock.tick(60) #60 FPS
+    clock.tick(FPS)
